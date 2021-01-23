cc926c77f8c5b6bf76278d56d7f3f75623d2b307
C36-HussainHaidarAlmousawi <70634196+C36-HussainHaidarAlmousawi@users.noreply.github.com>
2021-01-23 14:31:05 +0300

    sec2 done

--- App.js
+++ App.js
@@ -2,19 +2,62 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { Text } from "react-native";
 import styled from "styled-components";
+import Card from "./components/Card";
 
 export default function App() {
   return (
     <Container>
-      <Text>My first App!</Text>
-      <StatusBar style="auto" />
+      <TitleBar>
+        <Avatar source={require("./assets/avatar.png")} />
+        <Title>Welcome back,</Title>
+        <Name>Hussain</Name>
+        <StatusBar style="auto" />
+      </TitleBar>
+      <SubTitle>Continue Learning</SubTitle>
+      <Card />
     </Container>
   );
 }
 
+const SubTitle = styled.Text`
+  color: #b8bece;
+  font-weight: 600;
+  font-size: 15px;
+  margin-left: 20px;
+  margin-top: 50px;
+  text-transform: uppercase;
+`;
+
+const Avatar = styled.Image`
+  width: 44px;
+  height: 44px;
+  background: black;
+  border-radius: 22px;
+  margin-left: 20px;
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+
 const Container = styled.View`
   flex: 1;
   background-color: #f0f3f5;
-  justify-content: center;
-  align-items: center;
+`;
+
+const Title = styled.Text`
+  font-size: 16px;
+  color: #b8bece;
+  font-weight: 500;
+`;
+
+const Name = styled.Text`
+  font-size: 20px;
+  color: #3c4560;
+  font-weight: bold;
+`;
+
+const TitleBar = styled.View`
+  width: 100%;
+  margin-top: 50px;
+  padding-left: 80px;
 `;
