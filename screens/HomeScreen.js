f3807ad9a992e0bb64058ac9ae40ff41e83aa56f
hAlmousawi12 <almousawi2006@gmail.com>
2021-02-11 15:55:46 +0300

    meow

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -9,8 +9,8 @@ import {
 } from "react-native";
 import styled from "styled-components";
 import Card from "../components/Card";
-import { Icon } from "expo";
-import { Ionicons } from "@expo/vector-icons";
+// import { Icon } from "expo";
+// import { Ionicons } from "@expo/vector-icons";
 import { NotificationIcon } from "../components/Icons";
 import Logo from "../components/Logo";
 import Course from "../components/Course";
